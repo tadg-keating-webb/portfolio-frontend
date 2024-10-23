2a2bfe4273d8671098aab97b3d958e9a79ffb17c
Tadg Keating-Webb <tadgkeating@gmail.com>
2024-10-23 21:40:22 +0700

    Added CV link to Hero component

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -19,7 +19,7 @@
           delivering clean, maintainable code while solving complex challenges with creativity and precision.
         </p>
         <div class="flex justify-center my-6">
-          <a href="https://www.linkedin.com/in/tadg-keating-webb/" target="_blank">
+          <a href="https://www.linkedin.com/in/tadg-keating-webb/" target="_blank" v-tooltip="'LinkedIn Profile'">
             <button
               class="inline-flex text-white bg-secondary-600 border-0 py-4 px-6 focus:outline-none hover:bg-primary-500 rounded-lg text-lg"><svg
                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin"
@@ -29,13 +29,22 @@
               </svg>
             </button>
           </a>
-          <a href="https://github.com/tadg-keating-webb" target="_blank">
+          <a href="https://github.com/tadg-keating-webb" target="_blank" v-tooltip="'Github Profile'">
             <button
               class="ml-4 inline-flex text-gray-400 bg-secondary-600 border-0 py-4 px-6 focus:outline-none hover:bg-primary-500 hover:text-white rounded-lg text-lg"><svg
                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-behance"
-                viewBox="0 0 16 16">
+                fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
                   d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </button>
+          </a>
+          <a href="/cv.pdf" target="_blank" v-tooltip="'Curriculum Vitae'">
+            <button
+              class="ml-4 inline-flex text-gray-400 bg-secondary-600 border-0 py-4 px-6 focus:outline-none hover:bg-primary-500 hover:text-white rounded-lg text-lg">
+              <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-behance">
+                <path
+                  d="M22 24h-20v-24h14l6 6v18zm-7-23h-12v22h18v-16h-6v-6zm3 15v1h-12v-1h12zm0-3v1h-12v-1h12zm0-3v1h-12v-1h12zm-2-4h4.586l-4.586-4.586v4.586z" />
               </svg>
             </button>
           </a>
